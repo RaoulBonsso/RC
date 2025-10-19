073db1b13664d8216f0cbdad9a139c8f40258f2a
RaoulBonsso <raoulbonsso@gmail.com>
2025-10-19 03:05:33 +0200

    changes on content

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,20 +26,9 @@ const Hero = () => {
 
         {/* Description */}
         <p className="text-base sm:text-lg lg:text-2xl text-gray-600 tracking-wider max-w-[25rem] lg:max-w-[30rem]">
-         Chloé,
-
-Au cas où tu ne l'aurais pas compris, c'est toi, oui, toi qui occupes toutes mes pensées. Je suis, malgré moi, tombé amoureux de toi. Je sais que tu as souffert, et j'aurais aimé être là pour toi avant que ce mec ne te fasse tant de mal.
-
-Je comprends que je ne pourrai jamais vraiment partager mon cœur, et je sais que je ne serai peut-être jamais à la hauteur. Tu mérites tellement mieux, et même si tu ne l'as pas dit, je le ressens. Je ne suis pas si parfait, si beau, ni si riche. Mais je suis amoureux de la plus magnifique des femmes, celle avec qui je rêve de construire ma vie, d'avoir des enfants, et de passer mes soirées à parler et à te montrer à quel point je t'aime.
-
-Ce n'est pas simplement une lettre d'amour, c'est mon cœur que je mets à nu pour toi. Mon seul désir est de faire ton bonheur, car tu le mérites amplement. Je comprends que je dois accepter que nous ne soyons que des collègues, comme tu l'as dit tout à l'heure. Mais je veux que tu saches que je serai toujours là pour toi si tu en as besoin.
-
-Je pense qu'il est temps pour moi d'arrêter d'espérer être avec toi, comme tu me l'as dit. Cependant, je ne veux pas te perdre pour autant, donc j'accepterai tout ce que tu décideras pour nous.
-
-Ce site que j'ai créé, c'est pour toi, de A à Z. Je l'ai mis en ligne juste pour te montrer à quel point je suis fier de dire à tous que je t'aime, même si ce n'est pas réciproque. Le site est conçu dans tes couleurs préférées, en orange, car je sais que c'est ta couleur. Si tu le souhaites, tu pourras même le personnaliser à ta guise.
-
-Je sais que l'hébergement me coûte cher, mais pour toi, je décrocherais la lune, Chloé. Je t'aime profondément.
-
+          Au cas où tu ne l'aurais pas compris, c'est toi, oui toi Chloé. 
+          Je suis malgré moi tombé amoureux de toi. Je sais que tu n'es pas prête pour tout ça, 
+          mais je veux faire ma vie avec toi. Je pense à toi constamment et je veux être avec toi .
         </p>
 
         {/* Buttons */}
